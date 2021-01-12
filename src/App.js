74f7f96370ce9383d700df733a6b38186c41f5d7
yeojin <jeonyeojin1993@gmail.com>
2021-01-12 22:34:22 +0900

    [feat] 새 컴포넌트 만들기 -- MyName 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { Fragment, Component } from 'react';
+import MyName from './components/MyName';
+
 // 이렇게 import 하는 것은 webpack 을 사용하기 때문에 가능한건데
 // => 프로젝트 빌드시 webpack에서 파일 확장자에 따라서 다른 작업을 하게 됨
 
-// 컴포넌트 를 만드는 방법 두가지
-// 1. 클래스를 통해 만드는 방법
-// -- 클래스 형태로 만들어진 컴포넌트에는 꼭 render() 함수가 있어야 한다!
-// 2. 함수를 통해 만드는 방법
-// 여기선 함수를 통해 만들었다.
+/*
+
+ * 컴포넌트 를 만드는 방법 두가지
+   1. 클래스를 통해 만드는 방법
+      - 클래스 형태로 만들어진 컴포넌트에는 꼭 render() 함수가 있어야 한다!
+   2. 함수를 통해 만드는 방법
+   여기선 함수를 통해 만들었다.
+
+
+ */
+
 
 function App() {
 
@@ -16,7 +24,7 @@ function App() {
     backgroundColor: 'green',
     color: 'yellow'
   }
-  return ( // 이 리턴문 `안에 있는 코드가 JSX 다 !
+  return ( // 이 리턴문 안에 있는 코드가 JSX 다 !
 
     <Fragment>
       {/*
@@ -41,10 +49,13 @@ function App() {
       <div style={style}>
       괜히 "<div></div>" 하나 더 만들어 봤어 : Fragment 테그 사용해 보려고
       </div>
+      <MyName name="리액트" />
 
 
     </Fragment>
   );
 }
+
+
 
 export default App;
---
+++ src/components/MyName.js
@@ -0,0 +1,25 @@
+/*
+* 리액트 컴포넌트에서 다루는 데이터 2개
+1. props
+   - 부모 컴포넌트가 자식 컴포넌트에게 주는 값
+   - 자식 컴포넌트는 props를 받아오기만 하고 수정할 수는 없다.
+2. state
+   - 컴포넌트 내부에서 선언하며 내부에서 값을 변경할 수 있다.  
+*/
+
+import React, { Component } from 'react';
+
+class MyName extends Component 
+{
+    render() {
+        return (
+            <div>
+                안녕하세요. 제 이름은 <strong>{this.props.name}</strong>입니다.
+            </div>
+        );
+    }
+}
+
+export default MyName;
+// export 빼먹으면 아래와 같은 컴파일 에러 뜸
+// Attempted import error: './components/MyName' does not contain a default export (imported as 'MyName').
