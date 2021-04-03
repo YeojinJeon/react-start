818411232652362e8e2e25fa22e81244ceebd942
yeojin_jeon <yeojin_jeon@tmax.co.kr>
2021-04-03 16:09:50 +0900

    [feature] propsTypes -- props 검증 -- 필수 props지정 : isRequire -> 지정해주지 않았을경우 개발자 도구에서 에러메세지 발생 -- props 타입 지정

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
 /*
 화살표 함수를 이용한 함수형 컴포넌트
 */
 
-const MyComponent = ({name, children}) => {
+const MyComponent = ({name, age, children}) => {
     return (
         <div> 
             안녕하세요. 제 이름은 {name}입니다. <br/>
+            나이는 {age} 입니다. <br/>
             children 값은 {children}입니다.           
         </div>)
 };
@@ -15,6 +17,11 @@ const MyComponent = ({name, children}) => {
 MyComponent.defaultProps = {
     name: '전여진'
 };
+
+MyComponent.propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number.isRequired
+}
 
 export default MyComponent;
 
