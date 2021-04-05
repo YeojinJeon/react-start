ac1df55b18beb7f2e25ffde8ff9bbe2e86efa563
yeojin_jeon <yeojin_jeon@tmax.co.kr>
2021-04-05 17:46:47 +0900

    [feature] 함수형 컴포넌트에서 state 사용하기 : useState -- useState의 parameter 는 초기값 -- 반환은 배열 (크기 2) -- 1) 현재 state -- 2) state 를 바꾸어 주는 함수

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React, { Fragment, Component } from 'react';
 import MyName from './components/MyName';
 import MyComponent from './components/MyComponent';
 import Counter from './components/Counter';
+import Say from './components/Say';
 
 // 이렇게 import 하는 것은 webpack 을 사용하기 때문에 가능한건데
 // => 프로젝트 빌드시 webpack에서 파일 확장자에 따라서 다른 작업을 하게 됨
@@ -55,6 +56,7 @@ function App() {
       <MyComponent>리액트</MyComponent>
       
       <Counter />
+      <Say />
     </Fragment>
   );
 }
